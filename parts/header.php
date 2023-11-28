1c1425a0491a65cca10d75fc5c72b42c3f14ff5c
Nerhan Ajradini <a.nerhan@gmail.com>
2023-11-28 18:29:24 +0100

    Create header.php

---
+++ parts/header.php
@@ -0,0 +1,13 @@
+<?php
+
+echo <<< navigation
+    <nav> 
+    <div id="linksImHeader">
+    <a href="Index.php" class="aHeader">Start</a>
+    <a href="#" class="aHeader">Bestellung</a>
+    <a href="Kunde.php" class="aHeader">Kunde</a>
+    <a href="#" class="aHeader">Verarbeitung</a>
+    <a href="#" class="aHeader">Fahrer</a>
+    </div>
+    </nav>
+navigation;
